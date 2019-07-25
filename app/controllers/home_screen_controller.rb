9c16ee0436f6b28ebaceebaadf40391de62aeda7
toknowjoyman <vmanglani@mail.sfsu.edu>
2019-07-24 18:03:56 -0700

    removed buggy references

--- app/controllers/home_screen_controller.rb
+++ app/controllers/home_screen_controller.rb
@@ -2,7 +2,7 @@ class HomeScreenController < ApplicationController
   def index
     @count = Meal.count
     gon.count = @count
-    @created_at = Meal.created_at
-    gon.created_at= @created_at
+    # @created_at = Meal.created_at
+    # gon.created_at= @created_at
   end
 end
