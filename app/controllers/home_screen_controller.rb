12828166448305613b508af72947caa4dc6d04df
toknowjoyman <vmanglani@mail.sfsu.edu>
2019-07-25 19:44:31 -0700

    implemented alogrithm

--- app/controllers/home_screen_controller.rb
+++ app/controllers/home_screen_controller.rb
@@ -2,6 +2,9 @@ class HomeScreenController < ApplicationController
   def index
     @count = Meal.count
     gon.count = @count
+
+    @meals = Meal.all
+    gon.meals = @meals
     # @created_at = Meal.created_at
     # gon.created_at= @created_at
   end
