448569808cd83b7a69900be460db8d079f914275
toknowjoyman <vmanglani@mail.sfsu.edu>
2019-07-27 13:34:16 -0700

    fixed alg. bugs/edge-cases/mistakes, fixed ui bugs, added home links on all pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get 'home_screen/index'
 
+  get 'meals/new'
+
   resources :meals   # can also be called ImpactActions
 
   root 'home_screen#index'
