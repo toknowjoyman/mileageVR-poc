51a4127c83b5aeca77a09b4acb07fd6b9f8d2a17
toknowjoyman <vmanglani@mail.sfsu.edu>
2019-07-29 21:20:36 -0700

    User signup/login added, pending error display

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ group :development, :test do
 
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  get 'home_screen/index'
-
-  get 'meals/new'
-
-  resources :meals   # can also be called ImpactActions
 
   root 'home_screen#index'
+  get 'home_screen/index'
+  get 'sessions/new'
+  get 'meals/new'
+
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+
+
+
+
+
+  resources :meals   # can later be called ImpactActions
+
+  resources :users
+
+
 end
